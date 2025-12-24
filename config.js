aa6da83fd5d6ec3f9ee0bc69716b388d18e775da
mnjj88c2sy-max <mnjj88c2sy@privaterelay.appleid.com>
2025-12-24 07:13:11 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export const AI_ENABLED = true;
+
+export const AI_MODEL = "gpt-4.1-mini";
+export const AI_CONFIDENCE_MIN = 0.55;
+
+// sicurezza
+export const MIN_SL_ATR_MULT = 0.5;
+export const MAX_PARTIAL_CLOSE_PCT = 50;
